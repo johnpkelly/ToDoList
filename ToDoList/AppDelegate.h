d9f9087202876e57deb3222a144beeeff498c42f
John Kelly <johnpkelly@Johns-MacBook-Air.local>
2014-02-11 16:43:07 -0800

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by John Kelly on 2/11/14.
+//  Copyright (c) 2014 John Kelly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
